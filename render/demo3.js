47812ed88ad6629d4750d55cfe05bb24f2a1ffd3
quadrant26 <quadrant26@163.com>
2020-05-20 15:14:06 +0800

    打开子窗口

--- render/demo3.js
+++ render/demo3.js
@@ -1,6 +1,7 @@
 var {shell} = require('electron')
 
 var ctn = document.querySelector("#ctn");
+var childopen = document.querySelector("#childopen");
 
 ctn.onclick = function (e){
 
@@ -8,4 +9,8 @@ ctn.onclick = function (e){
     
     var href = this.getAttribute("href");
     shell.openExternal(href);
+}
+
+childopen.onclick = function (){
+    window.open('https://www.baidu.com/')
 }
