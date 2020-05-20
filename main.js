758fe9256a8c4ec94996f93116e678167ded2ca3
quadrant26 <quadrant26@163.com>
2020-05-20 13:43:15 +0800

    Hello World

--- main.js
+++ main.js
@@ -5,3 +5,12 @@ var BrowserWindow = electron.BrowserWindow; // 用来控制窗口引入
 
 // 需要打开的主窗口
 var mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 300, hieght: 300})
+    mainWindow.loadFile("index.html")
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    })
+})
