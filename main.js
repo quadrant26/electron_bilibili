c0bcc2181bc5d376a27433cf8d61df56fcba6200
quadrant26 <quadrant26@163.com>
2020-05-21 11:30:47 +0800

    快捷键注册和注销

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var electron = require('electron');
 
 var app = electron.app // 引入app
 var BrowserWindow = electron.BrowserWindow; // 用来控制窗口引入
+// 注册全局快捷键
+var globalShortcut = electron.globalShortcut;
 
 // 需要打开的主窗口
 var mainWindow = null;
@@ -15,7 +17,19 @@ app.on('ready', () => {
         }
     })
 
+    // 打开调试模式
     mainWindow.webContents.openDevTools();
+
+    // 注册快捷键
+    globalShortcut.register('ctrl+e', () => {
+        mainWindow.loadURL('https://www.baidu.com/')
+    })
+    // 查看快捷键是否注册成功
+    let isRegister = globalShortcut.isRegistered('ctrl+e') ? 'Regsiter Success' : 'Register fail';
+    console.log('---->' + isRegister)
+
+    // 注销全局快捷键
+
 
     require('./main/menu');
 
@@ -28,9 +42,15 @@ app.on('ready', () => {
     view.webContents.loadURL('https://www.baidu.com/')
     */
 
-    mainWindow.loadFile("demo3.html")
+    mainWindow.loadFile("demo6.html")
 
     mainWindow.on('closed', () => {
         mainWindow = null;
     })
+})
+
+//注销全局快捷键的监听
+app.on('will-quit', function (){
+    globalShortcut.unregister('ctrl+e');
+    globalShortcut.unregisterAll();
 })
